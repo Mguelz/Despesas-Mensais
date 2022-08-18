b337a637aafd70691594e8fd8213849484f5c5e2
Miguel Arcanjo <945507756m@gmail.com>
2022-08-17 21:35:25 -0300

    variavel determina a quantidade de entradas

--- src/DespesasAcumulativas/DespeAcumu.java
+++ src/DespesasAcumulativas/DespeAcumu.java
@@ -10,25 +10,30 @@ public class DespeAcumu {
 	 * imprimir para a visualização
 	 */
 	public static void main(String[] args) {
-
+		
 		Scanner teclado = new Scanner(System.in);
-
+		
+		
 		int x = 1;
-		int despesa = 0;
+		int despesaTotal = 0;
 		int qtd = 5; //quantidade de vezes que digitou
+		int qtdContas; // quantidade vezes que gerará sysout para entrada de dados
+		
+		
+		System.out.println("Digite a quantidade de contas a pagar: ");
+		qtdContas = Integer.parseInt(System.out.println("Digite a quantidade de despesas"));
 
-		for (x = 1; x < 10; x++) {
-			System.out.println("Digite sua despesa" + x++ + ":");
-			despesa = teclado.nextInt();
+		for (x = 0; x < qtdContas; x++) {
+			System.out.println("Digite sua despesa:"); // fazer laço for para ficar despesa1, despesa2 etc
+			despesaTotal = teclado.nextInt();
 		}
 
 		// soma
-		despesa += x++;
+		despesaTotal *= qtdContas;
 
 		// saida
-		System.out.println("Sua despesa no total foi " + despesa + ":");
+		System.out.println("Sua despesa no total foi " + despesaTotal + ":");
 
-		
 		teclado.close();
 	}
 
